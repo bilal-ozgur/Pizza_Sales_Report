20f8e1f27e9302b490d3138216308b43ef0b55ad
Bilal Talha Özgür <130503711+bilal-ozgur@users.noreply.github.com>
2023-09-13 23:33:07 +0200

    KPI and Chart requirements added

---
+++ Project_Solution/SQLQuery-Pizza-Sales.sql
@@ -0,0 +1,39 @@
+
+
+-- KPI’s REQUIREMENT
+-- We need to analyze key indicators for our pizza sales data to gain insights into our business performance. Specifically, we want to calculate the following metrics:
+
+
+
+
+-- 1. Total Revenue: The sum of the total price of all pizza orders.
+
+-- 2. Average Order Value: The average amount spent per order, calculated by dividing the total revenue by the total number of orders.
+
+-- 3. Total Pizzas Sold: The sum of the quantities of all pizzas sold.
+
+-- 4. Total Orders: The total number of orders placed.
+
+-- 5. Average Pizzas Per Order: The average number of pizzas sold per order, calculated by dividing the total number of pizzas sold by the total number of orders.
+
+
+
+
+-- CHARTS REQUIREMENT
+-- We would like to visualize various aspects of our pizza sales data to gain insights and understand key trends. We have identified the following requirements for creating charts:
+
+
+--  1. Hourly Trend for Total Pizzas Sold:
+
+--  2. Weekly Trend for Total Orders:
+
+--  3. Percentage of Sales by Pizza Category:
+
+--  4. Percentage of Sales by Pizza Size:
+
+--  5. Total Pizzas Sold by Pizza Category:
+
+--  6. Top 5 Best Sellers by Revenue, Total Quantity and Total Orders
+
+--  7. Bottom 5 Best Sellers by Revenue, Total Quantity and Total Orders
+
